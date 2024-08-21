75778f3a35e0d3f78194e942ab77b5e06d8eb98f
Charlie <charliegu@berkeley.edu>
2024-08-20 23:22:21 -0700

    Initial Commit

---
+++ microlearn/microlearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  microlearnApp.swift
+//  microlearn
+//
+//  Created by Charlie  on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct microlearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ microlearnUITests/microlearnUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  microlearnUITestsLaunchTests.swift
+//  microlearnUITests
+//
+//  Created by Charlie  on 8/20/24.
+//
+
+import XCTest
+
+final class microlearnUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
